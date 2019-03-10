6f619f5dd9c503c53f3a694c0bcdae8d3849a512
Ashwin Vaidyanathan <vaidashwin@gmail.com>
2019-03-09 18:41:24 -0700

    .gitignore and wip view stuff.

--- bwti/follow/views.py
+++ bwti/follow/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import BWTweeter
 
 def index(request):
-  return HttpResponse("wip")
+  all_twitters = BWTweeter.objects.order_by('-twitter_un')
+  output = ', '.join([str(t) for t in all_twitters])
+  return HttpResponse(output)
