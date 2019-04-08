de382b45a9221ebe7154292a59b72f632fc0b6e4
Ashwin Vaidyanathan <vaidashwin@gmail.com>
2019-04-07 18:56:15 -0600

    Apparently that's how python handles paths...

--- bwti/bwti/urls.py
+++ bwti/bwti/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('/', include('follow.urls')),
+    path('', include('follow.urls')),
     path('admin/', admin.site.urls),
 ]
