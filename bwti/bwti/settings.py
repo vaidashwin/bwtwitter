e82722a768cdca7d82eefd8c5c5aa3c3ddafcd18
Ashwin Vaidyanathan <vaidashwin@gmail.com>
2019-04-13 11:10:25 -0600

    Fixing additional settings stuff.

--- bwti/bwti/settings.py
+++ bwti/bwti/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = './static/'
+STATIC_ROOT = '/app/bwtwitter/'
 
 API_KEY = "8n9SrgUkKSpvLlFmJoYnwthBW"
 API_SECRET = "dMSEbplBNZdvtErA1G2S2DIduc3iBxsA5WMwOQkhhzW01aK4Am"
